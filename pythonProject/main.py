3614318922408f02a859e358293f7145a5e3bf6a
mustafabayarr <bayarmustafa151@gmail.com>
2021-03-13 22:02:26 +0300

    without brackets output

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,10 +1,14 @@
 import os
 import pymysql
 
-conn = pymysql.connect(host='localhost',user='root',password=None,db='test')
+conn = pymysql.connect(host='localhost',user='root',password=None,db='pythonTest')
 cur = conn.cursor()
 if cur:
     print("Connection Succcesfully")
 else:
     print("Connection Denied.")
 
+sql = "SELECT * FROM pythontest"
+cur.execute(sql)
+for rs in cur:
+    print(rs[0],rs[1])
